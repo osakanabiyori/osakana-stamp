da5b3f99b58b010b213f6084a966e4654c6687b4
yamannnu <b1015108@fun.ac.jp>
2019-04-17 13:45:03 +0900

    スタンプとスクリプトを追加

---
+++ slack-emoji-export.js
@@ -0,0 +1,25 @@
+var Slack = require('slack-node');
+var request = require('request');
+var fs = require('fs');
+
+apiToken = ""; // ここにAPIトークンを貼り付ける。
+slack = new Slack(apiToken);
+
+slack.api("emoji.list", function (err, response) {
+    for(key in response.emoji){
+        url = response.emoji[key];
+        //エイリアスは無視
+        if(url.match(/alias/)){
+            continue;
+        }
+
+        // 取得対象の拡張子
+        extention = url.match(/\.[^\.]+$/);
+
+        request
+        .get(url)
+        .on('response', function (res) {
+        })
+        .pipe(fs.createWriteStream('image/' + key + extention));
+    }
+});
